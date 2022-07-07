ce1afc5ec850704e899162a2b257f1c73f15ca8c
Masroor Ehsan <masroore@gmail.com>
2022-07-07 22:21:33 +0200

    layout

--- client/src/containers/Layout.tsx
+++ client/src/containers/Layout.tsx
@@ -3,36 +3,40 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Footer from '../components/Footer'
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 type Props = {
     children: JSX.Element[] | JSX.Element
     footer: any
 }
+const theme = createTheme()
 
 export default function Layout({ children, footer }: Props) {
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}
-        >
-            <CssBaseline />
+        <ThemeProvider theme={theme}>
+            <Container component='main' maxWidth='md'>
+                <CssBaseline />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        minHeight: '88vh',
+                    }}
+                >
+                    <Container component='main' sx={{ mt: 6 }}>
+                        <Typography variant='h2' component='h1' gutterBottom>
+                            Shorty
+                        </Typography>
+                        <Typography variant='h5' component='h2' gutterBottom>
+                            {'URL shortener made with Reactjs & FastAPI'}
+                        </Typography>
 
-            <Container component='main' sx={{ mt: 8, mb: 2 }} maxWidth='sm'>
-                <Typography variant='h2' component='h1' gutterBottom>
-                    Shorty
-                </Typography>
-                <Typography variant='h5' component='h2' gutterBottom>
-                    {'URL shortener made with Reactjs & FastAPI'}
-                </Typography>
-                <Typography variant='body1'>Sticky footer placeholder.</Typography>
-
-                {children}
+                        {children}
+                    </Container>
+                </Box>
             </Container>
-
             <Footer {...footer} />
-        </Box>
+        </ThemeProvider>
     )
 }
