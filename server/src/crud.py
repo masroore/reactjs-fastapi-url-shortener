56c80068c8ab79a4176520cb681ac8907183f749
Masroor Ehsan <masroore@gmail.com>
2022-07-07 13:43:13 +0200

    prevent duplicate URL

--- server/src/crud.py
+++ server/src/crud.py
@@ -1,23 +1,15 @@
-import secrets
-import string
-
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import exists
+from starlette.datastructures import URL
 
-from . import models, config
-
-
-def random_id(max_len: int = 5) -> str:
-    return "".join(
-        secrets.choice(string.ascii_letters + string.digits) for _ in range(max_len)
-    )
+from . import models, config, schemas, utils
 
 
 def generate_unique_short_id(db: Session, max_len: int = 5) -> str:
-    short_id = random_id(max_len)
+    short_id = utils.generate_random_id(max_len)
 
     while short_id_exists(db, short_id):
-        short_id = random_id(max_len)
+        short_id = utils.generate_random_id(max_len)
 
     return short_id
 
@@ -35,7 +27,7 @@ def short_id_exists(db: Session, short_id: str) -> bool:
     # return db.query(models.Url).filter(models.Url.short_id == short_id).count() > 0
 
 
-def delete(db: Session, id: int):
+def delete_url(db: Session, id: int):
     db.query(models.Url).filter(models.Url.id == id).delete()
     db.commit()
 
@@ -48,16 +40,35 @@ def find_by_secret(db: Session, short_id: str, secret: str) -> models.Url:
     )
 
 
+def find_by_target_url(db: Session, target_url: str) -> models.Url:
+    return db.query(models.Url).filter(models.Url.target_url == target_url).first()
+
+
 def update_clicks(db: Session, link_id: int):
     db.query(models.Url).filter_by(id=link_id).update({"clicks": models.Url.clicks + 1})
     db.commit()
 
 
 def insert_url(db: Session, target_url: str) -> models.Url:
+    target_url = utils.normalize_url(target_url)
+    link = find_by_target_url(db, target_url)
+    if link:
+        return link
+
     short_id = generate_unique_short_id(db, config.config().short_id_length)
-    secret = random_id(config.config().secret_length)
+    secret = utils.generate_random_id(config.config().secret_length)
     link = models.Url(target_url=target_url, short_id=short_id, secret=secret)
     db.add(link)
     db.commit()
     db.refresh(link)
     return link
+
+
+def get_url_info(link: models.Url) -> schemas.UrlInfo:
+    base_url = URL(config.config().base_url)
+    return schemas.UrlInfo(
+        target_url=link.target_url,
+        clicks=link.clicks,
+        url=str(base_url.replace(path=link.short_id)),
+        delete_url=str(base_url.replace(path=f"{link.short_id}/{link.secret}")),
+    )
