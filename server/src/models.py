a5b04e453eb83bfacc04eb6dbc7234259bc3bbc2
Masroor Ehsan <masroore@gmail.com>
2022-07-07 13:43:38 +0200

    timestamps in database rows

--- server/src/models.py
+++ server/src/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy.sql import func
 
 from .database import Base
 
@@ -12,3 +13,5 @@ class Url(Base):
     secret = Column(String, unique=True, index=True)
     is_active = Column(Boolean, default=True)
     clicks = Column(Integer, default=0)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
