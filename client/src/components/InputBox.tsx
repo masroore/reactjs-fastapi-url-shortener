f9468ccf2e3042cdab85fc27249419e952bc9274
Masroor Ehsan <masroore@gmail.com>
2022-07-07 22:21:59 +0200

    input box

--- client/src/components/InputBox.tsx
+++ client/src/components/InputBox.tsx
@@ -1,38 +1,39 @@
+import * as React from 'react'
 import Button from '@mui/material/Button'
 import TextField from '@mui/material/TextField'
 import Box from '@mui/material/Box'
 
 interface Props {
     value: string
-    onChange: (event: any) => void
-    onSubmit: (event: any) => void
+    onChange: (url: string) => void
+    onSubmit: () => void
 }
 
 const InputBox = ({ value, onChange, onSubmit }: Props) => {
-    const handleSubmit = (event: any) => {
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        onSubmit(event)
+        onSubmit()
     }
 
     return (
-        <div>
-            <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-                <form onSubmit={handleSubmit}>
-                    <TextField
-                        label='URL'
-                        id='filled-size-small'
-                        value={value}
-                        sx={{ m: 1, width: '35ch' }}
-                        onChange={(e) => onChange(e.target.value)}
-                        variant='filled'
-                    />
+        <>
+            <Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                <TextField
+                    margin='normal'
+                    required
+                    fullWidth
+                    autoFocus
+                    label='URL'
+                    id='url'
+                    value={value}
+                    onChange={(e: any) => onChange(e.target.value)}
+                />
 
-                    <Button variant='contained' disableElevation size='large' type='submit'>
-                        Shorten
-                    </Button>
-                </form>
+                <Button type='submit' fullWidth variant='contained' sx={{ mt: 0, mb: 3 }} disableElevation>
+                    Shorten
+                </Button>
             </Box>
-        </div>
+        </>
     )
 }
 
