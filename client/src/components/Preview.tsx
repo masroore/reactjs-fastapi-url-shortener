8085d587828f8b99f6dc98c5f2ee3584eeb58079
Masroor Ehsan <masroore@gmail.com>
2022-07-08 23:49:41 +0200

    Preview uses mui.Button

--- client/src/components/Preview.tsx
+++ client/src/components/Preview.tsx
@@ -1,6 +1,7 @@
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { useState } from 'react'
 import TinyLink from './TinyLink'
+import Button from '@mui/material/Button'
 
 interface Props {
     target_url: string
@@ -11,10 +12,13 @@ const Preview = ({ target_url, short_url }: Props) => {
 
     return (
         <>
+            <p>{short_url}</p>
             <CopyToClipboard text={short_url} onCopy={() => setCopied(true)}>
-                <span>Click to Copy: {short_url}</span>
-            </CopyToClipboard>
-            {copied ? <span style={{ color: 'red' }}>Copied.</span> : null}
+                <Button variant='contained' disableElevation>
+                    Copy URL to Clipboard
+                </Button>
+            </CopyToClipboard>{' '}
+            {copied && <span style={{ color: 'red' }}>Copied.</span>}
             <TinyLink target_url={target_url} />
         </>
     )
